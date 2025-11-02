852c807e4de20febde280aca4c183c23f0fb1320
Symphony of the Night <gamexgaming1997@gmail.com>
2025-11-02 12:46:51 +0800

    recommit

--- app/routes/components/Dropdown/Dropdown.tsx
+++ app/routes/components/Dropdown/Dropdown.tsx
@@ -2,39 +2,39 @@ import React from 'react'
 
 interface DropdownProps {
     iconTrigger: boolean
+    setIconTrigger: any
+    setPointerOff: any
 }
 
-const Dropdown = ({ iconTrigger }: DropdownProps ) => {
+const Dropdown = ({ iconTrigger, setIconTrigger, setPointerOff }: DropdownProps ) => {
   return (
         <div className={`absolute top-0 left-1/2 -translate-x-1/2 
             ${iconTrigger ? 'h-full transition-all duration-500 delay-[1000ms]' : 'h-[0%] transition-all duration-500 delay-[1200ms]'}
                 w-[100vw] bg-transparent backdrop-blur-[1px]
                 flex flex-row items-start overflow-hidden`}
             >
-              <div className={`${iconTrigger ? 'flex' : 'h-[0vh]'} w-[49.5%] relative`}>
+              <div className={`${iconTrigger ? 'flex' : 'h-[0vh]'} flex-1 bg-red-200 relative`}>
               </div>
-              <div
-                className={`${
-                  iconTrigger
+              <div className={`${ iconTrigger
                     ? 'h-[100vh] transition-all duration-500 delay-[500ms]'
                     : 'h-[0vh] transition-all duration-500 delay-[600ms]'
-                } flex justify-end w-[55%] right-0 overflow-hidden relative`}
+                } flex justify-end max-w-[950px] w-full right-0 relative`}
               >
                   <button
-                    className={`absolute bottom-[1%] left-[0%]
+                    className={`absolute bottom-[10px] left-[-75px]
                       bg-white h-[60px] w-[60px]
-                      transition-all duration-500
+                      transition-all duration-500 cursor-pointer
                       ${iconTrigger
                         ? 'opacity-100 delay-[1500ms] pointer-events-auto'
                         : 'opacity-0 delay-[0ms] pointer-events-auto'}
-                    `}
+                    sm:hidden xl:block `}
                     onTransitionEnd={() => {
                       if (!iconTrigger) setTimeout(() => setPointerOff(true), 500);
                     }}
                     onClick={() => setIconTrigger(!iconTrigger)}
                   >
                   </button>
-                <div className="bg-white w-[92.5%] transition-all duration-500">
+                <div className="bg-white w-[100%] transition-all duration-500">
                 </div>
               </div>
         </div>
--- app/routes/components/Navigation/Navigation.tsx
+++ app/routes/components/Navigation/Navigation.tsx
@@ -25,15 +25,15 @@ const Navigation = () => {
             <button className='hamburger-btn-trigger relative grid items-center justify-center w-[95px] min-h-[50px] cursor-pointer z-[10]'
             onClick={() => setIconTrigger(!iconTrigger)}
             >
-              <div className={`${iconTrigger ? 'hamburger-line-one-true' : 'hamburger-line-one-false'} bg-white absolute h-[2px] w-[80px] -translate-x-1/2 -translate-y-1/2 left-[50%] top-[30%] transition-all duration-[.2s] delay-750 flex items-center justify-center`}>
+              <div className={`${iconTrigger ? 'hamburger-line-one-true' : 'hamburger-line-one-false'} bg-white absolute h-[2px] w-[70px] -translate-x-1/2 -translate-y-1/2 left-[50%] top-[30%] transition-all duration-[.2s] delay-750 flex items-center justify-center`}>
                 <div className={`bg-black origin-left h-[2px] transition-all duration-[.5s] ${iconTrigger ? 'w-[100%]' : 'w-[0%]'}`}></div>
               </div>
-              <div className={`${iconTrigger ? 'hamburger-line-two-true' : 'hamburger-line-two-false'} bg-white absolute h-[2px] w-[80px] -translate-x-1/2 -translate-y-1/2 left-[50%] top-[65%] transition-all duration-[.2s] delay-750 flex items-center justify-center`}>
+              <div className={`${iconTrigger ? 'hamburger-line-two-true' : 'hamburger-line-two-false'} bg-white absolute h-[2px] w-[70px] -translate-x-1/2 -translate-y-1/2 left-[50%] top-[65%] transition-all duration-[.2s] delay-750 flex items-center justify-center`}>
                 <div className={`bg-black origin-left h-[2px] transition-all duration-[.5s] ${iconTrigger ? 'w-[100%]' : 'w-[0%]'}`}></div>
               </div>
             </button>
             {/* reusable dropdown */}
-            <Dropdown iconTrigger={iconTrigger} />
+            <Dropdown iconTrigger={iconTrigger} setIconTrigger={setIconTrigger}  setPointerOff={setPointerOff}/>
         </nav>
     </header> 
   )
